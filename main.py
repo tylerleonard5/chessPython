bb3a6f736c19fa8bdd6620aa7787b6317f83079b
tylerleonard5 <tylerleonard@ufl.edu>
2022-01-03 11:39:19 -0500

    pieces move

--- main.py
+++ main.py
@@ -24,13 +24,19 @@ def drawSquares(screen):
 
 
 def createEvenRow(screen, count):
-    for i in [1, 3, 5, 7]:
+    for i in range(8):
         
-        pygame.draw.rect(screen, 'gray', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
+        if(i%2 == 1):
+            pygame.draw.rect(screen, 'gray', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
+        else: 
+            pygame.draw.rect(screen, 'white', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
 
 def createOddRow(screen, count):
-    for i in [0, 2, 4, 6]:
-        pygame.draw.rect(screen, 'gray', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
+    for i in range(8):
+        if(i % 2 == 0):
+            pygame.draw.rect(screen, 'gray', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
+        else: 
+            pygame.draw.rect(screen, 'white', pygame.Rect(i * (WIDTH // 8), count * ((WIDTH // 8)), WIDTH//8, WIDTH//8))
 
 def drawPieces(screen, board):
     for r in range(8):
@@ -52,11 +58,30 @@ def main():
     gs = chessEngine.State()
 
     running = True
+    sqSelected = () #Keeps track of selected square
+    playerClicks = [] #keep track of player clicks (two tuples: [(x1,y1). (x2,y2)])
     while running:
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()
+                col = location[0] // (WIDTH // 8)
+                row = location[1] // (WIDTH // 8)
+                if sqSelected == (row,col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    print(sqSelected)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = chessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+
 
         drawSquares(screen)
         drawPieces(screen, gs.board)
